87f2ed18f9df7b421cfb01f8f819b815384d27b0
Shkvaldev <shkvalgrozny@gmail.com>
2025-03-01 22:06:00 +0500

    added publishing results

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import asyncio
-import json
-from aio_pika import connect_robust, Message
-from aio_pika.patterns import Master
+from aio_pika import connect_robust
 from loguru import logger
 
 from message_manager import MessageManager
@@ -18,21 +16,11 @@ async def main():
             settings.tasks_queue
         )
         
-        buffer = {}
-         
         # Hook for processing messages
         await queue.consume(
-            MessageManager(buffer=buffer).handle,
+            MessageManager(channel=channel).handle,
             no_ack=False
         )
-
-        if "result" in buffer:
-            await channel.default_exchange.publish(
-                Message(
-                    body=json.dumps(buffer["answer"]).encode(),
-                    ),
-                    routing_key=settings.tasks_statuses_queue,
-            )
 
         # Run forever
         await asyncio.Future()
--- message_manager/__init__.py
+++ message_manager/__init__.py
@@ -1,20 +1,44 @@
 from typing import Any, Dict
-from aio_pika import IncomingMessage
+import json
+import asyncio
+from aio_pika import IncomingMessage, Message
 from loguru import logger
 
 from tasks.llm import LLM
 from tasks.webwalker import WebWalker
+from config import settings
 
 class MessageManager:
     """
     Comes as router which choses which task to petform operations on message from rabbitmq.
     """
-    def __init__(self, buffer: Dict[str, Any]) -> None:
-        self.buffer = buffer
+    def __init__(self, channel) -> None:
+        self.channel = channel
         self.links = {
             "llm": LLM,
             "webwalker": WebWalker
         }
+
+    async def publish_answer(self, payload):
+        """
+        Publishes answer to rabbitmq.
+        """
+        # Declarating result queue for sure
+        await self.channel.declare_queue(
+            settings.tasks_statuses_queue
+        )
+        # Publishing
+        try:
+            await self.channel.default_exchange.publish(
+                Message(
+                    body=json.dumps(payload).encode(),
+                    ),
+                    routing_key=settings.tasks_statuses_queue,
+            )
+        except Exception as e:
+            err_msg = f"Failed to publish answer to queue: {e}"
+            logger.error(err_msg)
+            raise ValueError(err_msg)
 
     async def handle(self, message: IncomingMessage):
         async with message.process():
@@ -26,7 +50,14 @@ class MessageManager:
 
             for link in self.links:
                 if link == type_header:
-                    # Performing task and saving results to buffer
-                    self.buffer["result"] = await self.links[link]().perform(message=message)
+                    # Performing task and publishing answer to the result queue
+                    task = asyncio.create_task(
+                        self.links[link]().perform(message=message)
+                    )
+                    payload = await task
+                    try:
+                        await self.publish_answer(payload=payload)
+                    except Exception as e:
+                        logger.error(f"Failed to handle message from rabbitmq: {e}")
                     return
             logger.warning(f"Can not define worker for task `{message.headers.get('task_id')}`")
